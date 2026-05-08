c8cdc31baa9bf4ed82fa447ba1569069db18dec0
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-08 06:34:53 +0200

    added new tokens

--- js/lexer.js
+++ js/lexer.js
@@ -16,5 +16,28 @@ export function tokenizer(input) {
                 value: ')',
             })
         }
+        let NUMBERS = /[0-9]/;
+        if (NUMBERS.test(char)) {
+            let value = '';
+            while (NUMBERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({ type: 'number', value });
+            continue;
+        }
+        let LETTERS = /[a-z]/i;
+        if (LETTERS.test(char)) {
+            let value = '';
+            while (LETTERS.test(char)) {
+                value += char;
+                char = input[++current];
+            }
+            tokens.push({ type: 'name', value });
+            continue;
+        }
+        throw new TypeError('Unknown charecter' + char);
     }
+
+    return tokens;
 }
