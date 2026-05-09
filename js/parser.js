e7989389f5171dfa7eb6d9639903907ddaccbc95
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-09 15:31:41 +0200

    added parse statements for x86 and arm instructions

--- js/parser.js
+++ js/parser.js
@@ -635,5 +635,518 @@ export function parser(tokens) {
                 value: token.value,
             };
         }
+
+        if (token.type === 'mov') {
+            current++;
+
+            return {
+                type: 'MOV',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'add') {
+            current++;
+
+            return {
+                type: 'ADD',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sub') {
+            current++;
+
+            return {
+                type: 'SUB',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'mul') {
+            current++;
+
+            return {
+                type: 'MUL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'div') {
+            current++;
+
+            return {
+                type: 'DIV',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sqr') {
+            current++;
+
+            return {
+                type: 'SQR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'pow') {
+            current++;
+
+            return {
+                type: 'POW',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'cmp') {
+            current++;
+
+            return {
+                type: 'CMP',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'jmp') {
+            current++;
+
+            return {
+                type: 'JMP',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'jnz') {
+            current++;
+
+            return {
+                type: 'JNZ',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'inc') {
+            current++;
+
+            return {
+                type: 'INC',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'dec') {
+            current++;
+
+            return {
+                type: 'DEC',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'xor') {
+            current++;
+
+            return {
+                type: 'XOR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'and') {
+            current++;
+
+            return {
+                type: 'AND',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'or') {
+            current++;
+
+            return {
+                type: 'OR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'not') {
+            current++;
+
+            return {
+                type: 'NOT',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'shl') {
+            current++;
+
+            return {
+                type: 'SHL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sar') {
+            current++;
+
+            return {
+                type: 'SAR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'rol') {
+            current++;
+
+            return {
+                type: 'ROL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'ror') {
+            current++;
+
+            return {
+                type: 'ROR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'ret') {
+            current++;
+
+            return {
+                type: 'RET',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'int') {
+            current++;
+
+            return {
+                type: 'INT',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'syscall') {
+            current++;
+
+            return {
+                type: 'SYSCALL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'ldr') {
+            current++;
+
+            return {
+                type: 'LDR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'str') {
+            current++;
+
+            return {
+                type: 'STR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'orr') {
+            current++;
+
+            return {
+                type: 'ORR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'eor') {
+            current++;
+
+            return {
+                type: 'EOR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bic') {
+            current++;
+
+            return {
+                type: 'BIC',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'lsl') {
+            current++;
+
+            return {
+                type: 'LSL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'lsr') {
+            current++;
+
+            return {
+                type: 'LSL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'asr') {
+            current++;
+
+            return {
+                type: 'ASR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'tst') {
+            current++;
+
+            return {
+                type: 'TST',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'b') {
+            current++;
+
+            return {
+                type: 'B',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bl_op') {
+            current++;
+
+            return {
+                type: 'BL_OP',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bx_op') {
+            current++;
+
+            return {
+                type: 'BX_OP',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'adr') {
+            current++;
+
+            return {
+                type: 'ADR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sdiv') {
+            current++;
+
+            return {
+                type: 'SDIV',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'udiv') {
+            current++;
+
+            return {
+                type: 'UDIV',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bfi') {
+            current++;
+
+            return {
+                type: 'BFI',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'ubfx') {
+            current++;
+
+            return {
+                type: 'UBFX',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'cbz') {
+            current++;
+
+            return {
+                type: 'CBZ',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'cbnz') {
+            current++;
+
+            return {
+                type: 'CBNZ',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'lui') {
+            current++;
+
+            return {
+                type: 'LUI',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'auipc') {
+            current++;
+
+            return {
+                type: 'AUIPC',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'lw') {
+            current++;
+
+            return {
+                type: 'LW',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sw') {
+            current++;
+
+            return {
+                type: 'SW',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'ld') {
+            current++;
+
+            return {
+                type: 'LD',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'sd') {
+            current++;
+
+            return {
+                type: 'SD',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'addi') {
+            current++;
+
+            return {
+                type: 'ADDI',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'slt') {
+            current++;
+
+            return {
+                type: 'SLT',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'slti') {
+            current++;
+
+            return {
+                type: 'SLTI',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'jal') {
+            current++;
+
+            return {
+                type: 'JAL',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'jalr') {
+            current++;
+
+            return {
+                type: 'JALR',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'beq') {
+            current++;
+
+            return {
+                type: 'BEQ',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bne') {
+            current++;
+
+            return {
+                type: 'BNE',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'blt') {
+            current++;
+
+            return {
+                type: 'BLT',
+                value: token.value,
+            };
+        }
+
+        if (token.type === 'bge') {
+            current++;
+
+            return {
+                type: 'BGE',
+                value: token.value,
+            };
+        }
     }
 }
