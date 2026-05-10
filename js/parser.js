bd08cc4368d18700be428cf244359ac7d9f1aaa1
Sviat6239 <sviatoslav6239@gmail.com>
2026-05-10 21:25:10 +0200

    added all neccessery registers into registerTokens

--- js/parser.js
+++ js/parser.js
@@ -21,8 +21,37 @@ export function parser(tokens) {
         'print', 'call', 'import', 'export', 'return', 'break', 'continue'
     ]);
     const registerTokens = new Set([
-        'rax', 'rbx', 'rcx', 'rdx', 'rsp', 'sbp', 'rsi',
-    ])
+        // x86-64 General Purpose (64-bit)
+        'rax', 'rbx', 'rcx', 'rdx', 'rsp', 'rbp', 'rsi', 'rdi',
+        'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
+
+        // x86 (32-bit)
+        'eax', 'ebx', 'ecx', 'edx', 'esp', 'ebp', 'esi', 'edi',
+        'r8d', 'r9d', 'r10d', 'r11d', 'r12d', 'r13d', 'r14d', 'r15d',
+
+        // x86 (16-bit)
+        'ax', 'bx', 'cx', 'dx', 'sp', 'bp', 'si', 'di',
+        'r8w', 'r9w', 'r10w', 'r11w', 'r12w', 'r13w', 'r14w', 'r15w',
+
+        // x86 (8-bit)
+        'al', 'bl', 'cl', 'dl', 'ah', 'bh', 'ch', 'dh',
+        'spl', 'bpl', 'sil', 'dil',
+        'r8b', 'r9b', 'r10b', 'r11b', 'r12b', 'r13b', 'r14b', 'r15b',
+
+        // x86 Segment & Special
+        'cs', 'ds', 'es', 'fs', 'gs', 'ss',
+        'rip', 'eip', 'ip', 'rflags', 'eflags', 'flags',
+
+        // ARM64 (64-bit X registers)
+        'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10',
+        'x11', 'x12', 'x13', 'x14', 'x15', 'x16', 'x17', 'x18', 'x19', 'x20',
+        'x21', 'x22', 'x23', 'x24', 'x25', 'x26', 'x27', 'x28', 'x29', 'x30',
+
+        // ARM64 (32-bit W registers)
+        'w0', 'w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w8', 'w9', 'w10',
+        'w11', 'w12', 'w13', 'w14', 'w15', 'w16', 'w17', 'w18', 'w19', 'w20',
+        'w21', 'w22', 'w23', 'w24', 'w25', 'w26', 'w27', 'w28', 'w29', 'w30'
+    ]);
 
     function peek(offset = 0) {
         return tokens[current + offset];
