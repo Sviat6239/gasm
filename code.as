e41122acda347df37dffb1b481f91c06ae52ec54
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-08 13:02:39 +0200

    update code.as to use mutable variable declarations; enhance gasm.php to parse and display variable details

--- code.as
+++ code.as
@@ -1,6 +1,6 @@
-let i32:number = 54;
-let f32:float_num = 5.14;
-const f64:pi = 3.14;
+let mut:i32:number = 54;
+let mut:f32:float_num = 5.14;
+const umut:f64:pi = 3.14;
 echo (number);
 echo (float_num);
 echo (pi);
--- gasm.php
+++ gasm.php
@@ -15,12 +15,22 @@
 
     print_r($data);
 
-    foreach ($data as $token){
-        if ($token[0] == "let"){
-            print_r("let\n");
-        } elseif ($token[0] == "const"){
-            print_r("const\n");
-        } elseif ($token[0] == "echo"){
+    foreach ($data as $tokens){
+        if ($tokens[0] == "let"){
+            $fullToken = $tokens[1];
+
+            $parts = explode(":", $fullToken);
+
+            if (count($parts) == 3){
+                $mutability = $parts[0];
+                $name = $parts[1];
+                $type = $parts[2];
+                $value = $tokens[3];
+
+                echo "$mutability:$name:$type:$value\n";
+            }
+
+        } elseif ($tokens[0] == "echo"){
             print_r("echo\n");
         }
     }
