31af85e37194cdaed67abf8235d87d8de0ffdc5b
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-12 16:40:03 +0200

    refactor gasm.c to streamline line reading and tokenization; remove split function and integrate logic into main

--- gasm.c
+++ gasm.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void split(char *s, char *d){
-    char *ss = strtok(s, d);
-    while(ss != NULL){
-        printf("%s\n", ss);
-        ss = strtok(NULL, d);
+typedef struct {
+    char **tokens;
+    int token_count;
+} Line;
+
+int main() {
+    FILE *fptr = fopen("code.as", "r");
+    if (!fptr) { perror("Error"); return -1; }
+
+    Line *lines = NULL;
+    int line_count = 0;
+    char buffer[1024];
+
+    while (fgets(buffer, sizeof(buffer), fptr)) {
+
+        lines = realloc(lines, (line_count + 1) * sizeof(Line));
+        lines[line_count].tokens = NULL;
+        lines[line_count].token_count = 0;
+
+        char *token = strtok(buffer, " \t\n\r");
+        while (token != NULL) {
+            int t_cnt = lines[line_count].token_count;
+            lines[line_count].tokens = realloc(lines[line_count].tokens, (t_cnt + 1) * sizeof(char *));
+            
+            lines[line_count].tokens[t_cnt] = strdup(token);
+            lines[line_count].token_count++;
+            
+            token = strtok(NULL, " \t\n\r");
+        }
+        line_count++;
     }
-}
 
-int main(){
-
-    int size = 100;
-
-    char *s;
-    
-    FILE *fptr;
-
-    fptr = fopen("code.as", "r");
-
-    char myString[size];
-
-    while(fgets(myString, size, fptr)) {
-        s = myString;
-        char *d = " ";
-
-        split(s, d);
+    for (int i = 0; i < line_count; i++) {
+        printf("Line %d: ", i);
+        for (int j = 0; j < lines[i].token_count; j++) {
+            printf("[%s] ", lines[i].tokens[j]);
+        }
+        printf("\n");
     }
 
+    for (int i = 0; i < line_count; i++) {
+        for (int j = 0; j < lines[i].token_count; j++) {
+            free(lines[i].tokens[j]);
+        }
+        free(lines[i].tokens);
+    }
+    free(lines);
     fclose(fptr);
 
     return 0;
