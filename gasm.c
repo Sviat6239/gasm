321d7bdf81c53f175b09fe8ff8331727209ca1a0
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-12 15:01:09 +0200

    refactor gasm.c to improve string handling; replace hardcoded size with variable and enhance split function

--- gasm.c
+++ gasm.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+void split(char *s, char *d){
+    char *ss = strtok(s, d);
+    while(ss != NULL){
+        printf("%s\n", ss);
+        ss = strtok(NULL, d);
+    }
+}
 
 int main(){
+
+    int size = 100;
+
+    char *s;
     
     FILE *fptr;
 
     fptr = fopen("code.as", "r");
 
-    char myString[100];
+    char myString[size];
 
-    while(fgets(myString, 100, fptr)) {
-    printf("%s", myString);
+    while(fgets(myString, size, fptr)) {
+        s = myString;
+        char *d = " ";
+
+        split(s, d);
     }
 
     fclose(fptr);
