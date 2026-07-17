2309c4cbbe5b5c94271276de20528e0f0195e362
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-17 15:33:43 +0200

    feat: update lexer and parser headers with token and AST node definitions

--- frontend/lexer.h
+++ frontend/lexer.h
@@ -4,11 +4,39 @@
 typedef enum{
     TOKEN_INT,
     TOKEN_ECHO,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_DIV,
+    TOKEN_MULT,
+    TOKEN_LET,
+    TOKEN_EQUAL,
+    TOKEN_SEMICOLON,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN,
     TOKEN_EOF,
-
+    TOKEN_MUT,
+    TOKEN_UMUT,
+    TOKEN_I64,
+    TOKEN_I32,
+    TOKEN_F64,
+    TOKEN_F32,
+    TOKEN_STR,
+    TOKEN_IDENTIFIER
 } TokenType;
 
 typedef struct{
     TokenType type;
-    char value[64];
+    int value;
+    char name[64];
 } Token;
+
+typedef struct {
+    Token* tokens;
+    int count;
+} TokenList;
+
+TokenList lex(const char* source);
+
+void print_tokens(TokenList* list);
+
+#endif
---
+++ frontend/parser.h
@@ -0,0 +1,9 @@
+#ifdef PARSER_H
+#endif PARSER_H
+
+typedef enum {
+    AST_NUMBER,
+    AST_ECHO,
+    AST_PROGRAM
+} ASTNode;
+
