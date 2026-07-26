34149c035094183467667e5c0f828fc85dd2a02c
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-26 21:22:36 +0200

    feat: enhance virtual machine with new opcodes for label handling and stack printing

--- backend/vm/bugvm.c
+++ backend/vm/bugvm.c
@@ -3,38 +3,42 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define OP_PUSH         0x0001 // push value on the stack
-#define OP_PUSH_INT     0X0002 // push int value on the stack
-#define OP_PUSH_STR     0x0003 // push str value on the stack
-#define OP_PUSH_FLOAT   0x0004 // push float value on the stack
-#define OP_POP          0x0005 // pop value from the stack
-#define OP_ADD          0x0006 // add two last values from the stack
-#define OP_SUB          0x0007 // sub two last values from the stack
-#define OP_MUL          0x0008 // mul two last values from the stack
-#define OP_DIV          0x0009 // div two last values from the stack
-#define OP_PRINT        0x000A // print the value from the stack
-#define OP_INPUT        0x000B // read value to the stack
-#define OP_STORE        0x000C // store value from the stack in a local variable
-#define OP_LOAD         0x000D // put a value from locals onto stack
-#define OP_CMP          0x000E // compare two top values from stack and set flags
-#define OP_JMP          0x000F // unconditional jump to a target line
-#define OP_JNZ          0x0010 // jump to a target line if flag is not zero
-#define OP_JZ           0x0011 // jump to a target line if flag is zero
-#define OP_JNE          0x0012 // jump if not equal (flags != FL_EQ)
-#define OP_JE           0x0013 // jump if equal (flags == FL_EQ)
-#define OP_JGE          0x0014 // jump if greater or equal (flags == FL_GT || flags == FL_EQ)
-#define OP_JG           0x0015 // jump if greater (flags == FL_GT)
-#define OP_JLE          0x0016 // jump if less or equal (flags == FL_LT || flags == FL_EQ)
-#define OP_JL           0x0017 // jump if less (flags == FL_LT)
-#define OP_AND          0x0018 // logical AND
-#define OP_OR           0x0019 // logical OR
-#define OP_NOR          0x001A // logical NOT
-#define OP_CALL         0x001B // call function
-#define OP_RET          0x001C // return from function
-#define OP_STRLEN       0x001D // string length
-#define OP_STRCMP       0x001E // compare string
-#define OP_LABEL        0x001F // create label
-#define OP_HALT         0xFFFF // halt the program
+#define OP_PUSH             0x0001 // push value on the stack
+#define OP_PUSH_INT         0x0002 // push int value on the stack
+#define OP_PUSH_STR         0x0003 // push str value on the stack
+#define OP_PUSH_FLOAT       0x0004 // push float value on the stack
+#define OP_POP              0x0005 // pop value from the stack
+#define OP_ADD              0x0006 // add two last values from the stack
+#define OP_SUB              0x0007 // sub two last values from the stack
+#define OP_MUL              0x0008 // mul two last values from the stack
+#define OP_DIV              0x0009 // div two last values from the stack
+#define OP_PRINT            0x000A // print the value from the stack
+#define OP_INPUT            0x000B // read value to the stack
+#define OP_STORE            0x000C // store value from the stack in a local variable
+#define OP_LOAD             0x000D // put a value from locals onto stack
+#define OP_CMP              0x000E // compare two top values from stack and set flags
+#define OP_JMP              0x000F // unconditional jump to a target line
+#define OP_JNZ              0x0010 // jump to a target line if flag is not zero
+#define OP_JZ               0x0011 // jump to a target line if flag is zero
+#define OP_JNE              0x0012 // jump if not equal (flags != FL_EQ)
+#define OP_JE               0x0013 // jump if equal (flags == FL_EQ)
+#define OP_JGE              0x0014 // jump if greater or equal (flags == FL_GT || flags == FL_EQ)
+#define OP_JG               0x0015 // jump if greater (flags == FL_GT)
+#define OP_JLE              0x0016 // jump if less or equal (flags == FL_LT || flags == FL_EQ)
+#define OP_JL               0x0017 // jump if less (flags == FL_LT)
+#define OP_AND              0x0018 // logical AND
+#define OP_OR               0x0019 // logical OR
+#define OP_NOT              0x001A // logical NOT
+#define OP_CALL             0x001B // call function
+#define OP_RET              0x001C // return from function
+#define OP_STRLEN           0x001D // gets first string value from the stack and then return its len
+#define OP_STRCMP           0x001E // compare string
+#define OP_LABEL            0x001F // create label
+#define OP_PRT_STACK        0x0020 // print last value on stack and it position
+#define OP_PRT_ALL_STACK    0x0021 // print all value on the stack and their positions
+#define OP_PRT_LOCAL        0x0022 // print last value in local storage and it position
+#define OP_PRT_ALL_LOCAL    0x0023 // print all value in local storage and their positions
+#define OP_HALT             0xFFFF // halt the program
 
 typedef enum {
     FL_EQ,
@@ -64,7 +68,12 @@ typedef struct{
     int token_count;
 } Line;
 
-#define STACK_SIZE (1024 * 1024 * 16)
+typedef struct {
+    char *name;
+    int line_count;
+} Label;
+
+#define STACK_SIZE (1024 * 1024 * 2)
 #define LOCAL_SIZE (1024 * 1024 * 16)
 Object stack[STACK_SIZE];
 Object locals[LOCAL_SIZE];
@@ -168,7 +177,7 @@ void parse_line(const char *buffer, Line *line)
 }
 
 int main(){
-    FILE *fptr = fopen("bytecode001.bbin", "r");
+    FILE *fptr = fopen("bytecode005.bbin", "r");
     if (!fptr)
     {
         perror("Error opening input file 'code.as'");
@@ -178,6 +187,9 @@ int main(){
     Line *lines = NULL;
     int line_count = 0;
     char buffer[1024];
+
+    Label *labels = NULL;
+    int label_count = 0;
 
     // ==================== READING AND TOKENIZING PHASE ====================
     printf("=== Reading and tokenizing code.as ===\n");
@@ -220,6 +232,25 @@ int main(){
 
     char *value;
 
+    // ==================== COLLECTS ALL LABELS ====================
+    for (int i = 0; i < line_count; i++) {
+    if (lines[i].token_count == 0) continue;
+
+    int opcode = (int)strtol(lines[i].tokens[0], NULL, 0);
+    
+    if (opcode == OP_LABEL) {
+        if (lines[i].token_count < 2) {
+            printf("Error: OP_LABEL requires a name/identifier!\n");
+            exit(1);
+        }
+
+        labels = realloc(labels, (label_count + 1) * sizeof(Label));
+        labels[label_count].name = strdup(lines[i].tokens[1]);
+        labels[label_count].line_count = i;
+        label_count++;
+    }
+}
+
     // ==================== EXECUTE LOOP ====================
     printf("\n=== Execution ===\n");
     for (ip = 0; ip < line_count; ip++) {
@@ -231,6 +262,12 @@ int main(){
         switch (opcode) {
             case OP_PUSH: {
                 int val = (int)strtol(lines[ip].tokens[1], NULL, 0);
+                push_int(val);
+                break;
+            }
+
+            case OP_PUSH_INT: {
+                double val = (int)strtol(lines[ip].tokens[1], NULL, 0);
                 push_int(val);
                 break;
             }
@@ -374,7 +411,7 @@ int main(){
                     } else if (obj.type == VAL_STR) {
                         printf("%s\n", obj.value.as_str);
                     } else if (obj.type == VAL_FLOAT) {
-                        printf("%s\n", obj.value.as_float);
+                        printf("%f\n", obj.value.as_float);
                     }
                 }
                 break;
@@ -410,7 +447,8 @@ int main(){
 
             case OP_STORE:{
                 if (lines[ip].token_count < 2){
-                    printf("Error: required argument!");
+                    printf("Error: required argument!\n");
+                    printf("Error on %d line\n", line_count);
                     exit(1);
                 }
                 int idx = (int)strtol(lines[ip].tokens[1], NULL, 0);
@@ -430,7 +468,8 @@ int main(){
 
             case OP_LOAD:{
                 if (lines[ip].token_count < 2){
-                    printf("Error: ruquired argumnet!");
+                    printf("Error: ruquired argument!\n");
+                    printf("Error on %d line\n", line_count);
                     exit(1);
                 }
                 int idx = (int)strtol(lines[ip].tokens[1], NULL, 0);
@@ -472,15 +511,26 @@ int main(){
 
             case OP_JMP: {
                 if (lines[ip].token_count < 2) {
-                    printf("Runtime Error: OP_JMP requires a target line!\n");
+                    printf("Runtime Error: OP_JMP requires a target label!\n");
                     return 1;
                 }
-                int target_line = (int)strtol(lines[ip].tokens[1], NULL, 0);
-                if (target_line < 0 || target_line >= line_count) {
-                    printf("Runtime Error: JMP target out of bounds!\n");
+                
+                char *target_name = lines[ip].tokens[1];
+                int target_line = -1;
+
+                for (int i = 0; i < label_count; i++) {
+                    if (strcmp(labels[i].name, target_name) == 0) {
+                        target_line = labels[i].line_count;
+                        break;
+                    }
+                }
+
+                if (target_line == -1) {
+                    printf("Runtime Error: Label '%s' not found!\n", target_name);
                     return 1;
                 }
-                ip = target_line - 1; 
+
+                ip = target_line - 1;
                 break;
             }
 
@@ -579,13 +629,85 @@ int main(){
             case OP_RET:
                 break;
 
-            case OP_STRCMP:
+            case OP_STRCMP:{
+                Object a = pop();
+                Object b = pop();
+
+                int cmp = strcmp(a.value.as_str, b.value.as_str);
+                push_int(cmp);
+
+                break;
+            }
+
+            case OP_STRLEN: {
+                Object a = pop();
+
+                if (a.type == VAL_INT) {
+                    printf("Error: invalid type: INT!\n");
+                    return 1;
+                } else if (a.type == VAL_FLOAT) {
+                    printf("Error: invalid type: FLOAT!\n");
+                    return 1;
+                } else if (a.type == VAL_STR) {
+                    if (a.value.as_str == NULL) {
+                        push_int(0);
+                    } else {
+                        int len = (int)strlen(a.value.as_str);
+                        push_int(len);
+                    }
+                } else {
+                    printf("Error: unknown type for OP_STRLEN!\n");
+                    return 1;
+                }
+                break;
+            }
+
+            case OP_LABEL: {
+                break;
+            }
+
+            case OP_PRT_STACK: {
+                if (sp < 0) {
+                    printf("Error: Stack is empty!\n");
+                } else {
+                    Object obj = stack[sp];
+                    printf("Current stack position: %d, value: ", sp+1);
+                    
+                    if (obj.type == VAL_INT) {
+                        printf("%d\n", obj.value.as_int);
+                    } else if (obj.type == VAL_STR) {
+                        printf("%s\n", obj.value.as_str);
+                    } else if (obj.type == VAL_FLOAT) {
+                        printf("%f\n", obj.value.as_float);
+                    }
+                }
+                break;
+            }
+
+            case OP_PRT_LOCAL:
                 break;
 
-            case OP_STRLEN:
+            case OP_PRT_ALL_STACK:{
+                if (sp < 0){
+                    printf("Error: Stack is empty!\n");
+                } else {
+                    Object obj = stack[sp];
+                    for (int i; i <= sp; i++){
+                        printf("Current stack position: %d, value: ", sp);
+                    
+                        if (obj.type == VAL_INT) {
+                            printf("%d\n", obj.value.as_int);
+                        } else if (obj.type == VAL_STR) {
+                            printf("%s\n", obj.value.as_str);
+                        } else if (obj.type == VAL_FLOAT) {
+                            printf("%f\n", obj.value.as_float);
+                        }
+                    }
+                }
                 break;
+            }
 
-            case OP_LABEL:
+            case OP_PRT_ALL_LOCAL:
                 break;
 
             case OP_HALT:
@@ -609,6 +731,12 @@ int main(){
         free(lines[i].tokens);
     }
     free(lines);
+
+    for (int i = 0; i < label_count; i++) {
+        free(labels[i].name);
+    }
+    free(labels);
+
     fclose(fptr);
 
     return 0;
